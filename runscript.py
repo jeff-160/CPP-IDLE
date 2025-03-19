9a934ae436084ad9d2690839d98eeb11cc83b5d0
jeff160 <141229412+jeff-160@users.noreply.github.com>
2025-03-20 00:09:21 +0800

    Add files via upload

--- runscript.py
+++ runscript.py
@@ -154,46 +154,6 @@ class ScriptBinding:
             log(f'Error: {e}')
             
         return 'break'
-    
-        code = self.checksyntax(filename)
-        if not code:
-            return 'break'
-        if not self.tabnanny(filename):
-            return 'break'
-        if customize:
-            title = f"Customize {self.editwin.short_title()} Run"
-            run_args = CustomRun(self.shell.text, title,
-                                 cli_args=self.cli_args).result
-            if not run_args:  # User cancelled.
-                return 'break'
-        self.cli_args, restart = run_args if customize else ([], True)
-        interp = self.shell.interp
-        if pyshell.use_subprocess and restart:
-            interp.restart_subprocess(
-                    with_cwd=False, filename=filename)
-        dirname = os.path.dirname(filename)
-        argv = [filename]
-        if self.cli_args:
-            argv += self.cli_args
-        interp.runcommand(f"""if 1:
-            __file__ = {filename!r}
-            import sys as _sys
-            from os.path import basename as _basename
-            argv = {argv!r}
-            if (not _sys.argv or
-                _basename(_sys.argv[0]) != _basename(__file__) or
-                len(argv) > 1):
-                _sys.argv = argv
-            import os as _os
-            _os.chdir({dirname!r})
-            del _sys, argv, _basename, _os
-            \n""")
-        interp.prepend_syspath(filename)
-        # XXX KBK 03Jul04 When run w/o subprocess, runtime warnings still
-        #         go to __stderr__.  With subprocess, they go to the shell.
-        #         Need to change streams in pyshell.ModifiedInterpreter.
-        interp.runcode(code)
-        return 'break'
 
     def getfilename(self):
         """Get source filename.  If not saved, offer to save (or create) file
